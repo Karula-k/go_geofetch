af3030163dc5b7b1ea709c22de4c3435ee888b62
Karula-k <d.fikriirsyad@gmail.com>
2025-10-14 23:09:04 +0700

    migrate down wrong command

--- db/migrations/000003_alter_timestamp.down.sql
+++ db/migrations/000003_alter_timestamp.down.sql
@@ -4,8 +4,8 @@ ALTER TABLE vehicle_location ADD COLUMN timestampZ TIMESTAMPTZ;
 -- Step 2: Populate the new timestamp column with the BIGINT values (epoch time)
 UPDATE vehicle_location 
 SET timestampZ = TO_TIMESTAMP(timestamp);
--- Step 3: Drop the timestamp_bigint column (which was used to store BIGINT)
-ALTER TABLE vehicle_location DROP COLUMN timestamp_bigint;
+-- Step 3: Drop the timestamp  column (which was used to store BIGINT)
+ALTER TABLE vehicle_location DROP COLUMN timestamp ;
 
 -- Step 4: Rename the new column to 'timestamp'
 ALTER TABLE vehicle_location RENAME COLUMN timestampZ TO timestamp;
