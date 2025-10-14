879cd424bd8e6712784eb536ac3c07af19f6cab6
Karula-k <d.fikriirsyad@gmail.com>
2025-10-14 23:05:05 +0700

    drop unique

--- db/migrations/000003_alter_timestamp.down.sql
+++ db/migrations/000003_alter_timestamp.down.sql
@@ -3,8 +3,7 @@ ALTER TABLE vehicle_location ADD COLUMN timestampZ TIMESTAMPTZ;
 
 -- Step 2: Populate the new timestamp column with the BIGINT values (epoch time)
 UPDATE vehicle_location 
-SET timestampZ = TO_TIMESTAMP(timestamp_bigint);
-
+SET timestampZ = TO_TIMESTAMP(timestamp);
 -- Step 3: Drop the timestamp_bigint column (which was used to store BIGINT)
 ALTER TABLE vehicle_location DROP COLUMN timestamp_bigint;
 
